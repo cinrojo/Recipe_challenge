222fd1a95b8b608ad639de1cbc2c53bc34aa18ed
cinrojo <c.rojo@itecriocuarto.org.ar>
2024-08-26 22:48:45 -0300

    correction user

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -5,7 +5,7 @@ const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(localStorage.getItem('user')|| null);
 
   const login = (username) => {
     setUser(username);
